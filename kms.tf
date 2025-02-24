30caa099358dc1a2d7d5c443cee4609596efd310
incubator4 <aries0robin@gmail.com>
2025-02-24 14:59:47 +0800

    feat: update KMS configuration to support external key ring and add output variables for key ring and crypto key names

--- kms.tf
+++ kms.tf
@@ -34,11 +34,11 @@ resource "google_kms_crypto_key" "primary" {
 }
 
 resource "google_kms_crypto_key_iam_binding" "primary" {
-  crypto_key_id = google_kms_crypto_key.primary[0].id
+  crypto_key_id = var.create_key_ring ? google_kms_key_ring.primary[0].id : data.google_kms_key_ring.external[0].id
   role          = "roles/cloudkms.signerVerifier"
   members       = ["serviceAccount:${google_service_account.cloud_run_service_account[0].email}"]
 }
 
 data "google_kms_crypto_key_latest_version" "primary" {
-  crypto_key = google_kms_crypto_key.primary[0].id
+  crypto_key = var.create_key_ring ? google_kms_key_ring.primary[0].id : data.google_kms_key_ring.external[0].id
 }
---
+++ output.tf
@@ -0,0 +1,7 @@
+output "key_ring_name" {
+  value = local.key_ring_name
+}
+
+output "crypto_key_name" {
+  value = local.crypto_key_name
+}
